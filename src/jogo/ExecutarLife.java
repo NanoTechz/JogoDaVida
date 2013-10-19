c7b81a9d7d1f5af00ffe0a51ff11a9dd410bec48
Nanson Augusto <nanson.augusto@hotmail.com>
2013-10-19 15:30:17 -0300

    Finalindo estrutura básica.

--- src/jogo/ExecutarLife.java
+++ src/jogo/ExecutarLife.java
@@ -4,21 +4,80 @@ import javax.swing.JOptionPane;
 
 public class ExecutarLife {
 
-
 	public static void main(String[] args) {
-		int tamanho = 10;
-		int aux[][] =  new int[tamanho][tamanho];
+		int tamanho = receberTamanho();
+		int qtdSimulacoes;
+		int aux[][] = new int[tamanho][tamanho];
 		Life life = new Life(tamanho);
-		/*
-		for(int i = 0; i < tamanho; i++){
-			for(int j = 0; j < tamanho; j++){
-				aux[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Digite 0=morte ou 1=vida"));
+
+		for (int i = 0; i < tamanho; i++) {
+			for (int j = 0; j < tamanho; j++) {
+				aux[i][j] = receberValorCelula(i, j);
 			}
 		}
-		*/
-		//life.inicializar(aux);
-		life.inicializar();
-		life.simulaVida(5);
+		
+		qtdSimulacoes = receberInteiro("Digite a quantidade de simulações");
+		
+		life.inicializar(aux);
+		life.simulaVida(qtdSimulacoes);
+	}
+
+	public static int receberInteiro(String mensagem) {
+		try {
+			return Integer.parseInt(JOptionPane
+					.showInputDialog(mensagem));
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null,
+					"Erro! Insira um valor válido.");
+			return receberInteiro(mensagem);
+		}
+	}
+
+	public static int receberTamanho() {
+		return receberTamanho(5);
+	}
+
+	public static int receberTamanho(int erro) {
+		try {
+			return Integer.parseInt(JOptionPane
+					.showInputDialog("Digite o tamanho do quadrado:"));
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null,
+					"Erro na conversão, inicializando matriz padrão " + erro
+							+ "x" + erro + ".");
+			return erro;
+		}
+	}
+
+	public static int receberValorCelula(int i, int j) {
+		try {
+			int temp = Integer.parseInt(JOptionPane.showInputDialog("P(" + i
+					+ "," + j + ") Digite 0=morte ou 1=vida"));
+
+			if ((temp >= 0) && (temp <= 1)) { // Valor válido
+				return temp;
+			} else {
+				if (erro("Valor inserido != de 0 ou 1, deseja continuar?"))
+					return receberValorCelula(i, j);
+				else
+					return 0;
+			}
+		} catch (Exception e) {
+			if (erro("Erro na conversão, deseja continuar?"))
+				return receberValorCelula(i, j);
+			else
+				return 0;
+		}
+	}
+
+	public static boolean erro(String mensagem) {
+		int opcao = JOptionPane.showConfirmDialog(null, mensagem);
+
+		if (opcao == JOptionPane.OK_OPTION) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
